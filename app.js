33007853ddd426a6828cb3046e1d81da896d0832
emanresuhg <9y4fbsfhhj@privaterelay.appleid.com>
2026-03-14 17:02:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -108,3 +108,199 @@ loadPassages()
 
 
 loadPassages()
+
+
+let currentSetIndex=null
+
+function loadSets(){
+
+const sets = JSON.parse(localStorage.getItem("wordSets")) || []
+
+const list=document.getElementById("setList")
+
+if(!list) return
+
+list.innerHTML=""
+
+sets.forEach((set,i)=>{
+
+const div=document.createElement("div")
+
+div.className="wordCard"
+
+div.innerHTML=`
+
+<b>${set.name}</b>
+
+<div class="wordActions">
+
+<button class="smallBtn" onclick="openSet(${i})">열기</button>
+
+<button class="smallBtn" onclick="deleteSet(${i})">삭제</button>
+
+</div>
+
+`
+
+list.appendChild(div)
+
+})
+
+}
+
+function addSet(){
+
+const name=document.getElementById("setName").value
+
+if(!name) return
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+sets.push({name:name,words:[]})
+
+localStorage.setItem("wordSets",JSON.stringify(sets))
+
+document.getElementById("setName").value=""
+
+loadSets()
+
+}
+
+function deleteSet(i){
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+sets.splice(i,1)
+
+localStorage.setItem("wordSets",JSON.stringify(sets))
+
+loadSets()
+
+}
+
+function openSet(i){
+
+currentSetIndex=i
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+document.getElementById("currentSetTitle").innerText=sets[i].name
+
+document.getElementById("wordSection").style.display="block"
+
+loadWords()
+
+}
+
+function backToSets(){
+
+document.getElementById("wordSection").style.display="none"
+
+}
+
+function loadWords(){
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+const words=sets[currentSetIndex].words
+
+const list=document.getElementById("wordList")
+
+list.innerHTML=""
+
+words.sort((a,b)=>a.eng.localeCompare(b.eng))
+
+words.forEach((w,i)=>{
+
+const div=document.createElement("div")
+
+div.className="wordCard"
+
+div.innerHTML=`
+
+<b>${w.eng}</b> : ${w.mean.join(", ")}
+
+<div class="wordActions">
+
+<button class="smallBtn" onclick="speakWord('${w.eng}')">🔊</button>
+
+<button class="smallBtn" onclick="toggleFavorite(${i})">${w.favorite?"★":"☆"}</button>
+
+<button class="smallBtn" onclick="deleteWord(${i})">삭제</button>
+
+</div>
+
+`
+
+list.appendChild(div)
+
+})
+
+}
+
+function addWord(){
+
+const eng=document.getElementById("englishWord").value
+
+const meanings=document.getElementById("meanings").value
+
+if(!eng || !meanings) return
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+const meanList=meanings.split(",")
+
+sets[currentSetIndex].words.push({
+
+eng:eng,
+
+mean:meanList,
+
+favorite:false
+
+})
+
+localStorage.setItem("wordSets",JSON.stringify(sets))
+
+document.getElementById("englishWord").value=""
+document.getElementById("meanings").value=""
+
+loadWords()
+
+}
+
+function deleteWord(i){
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+sets[currentSetIndex].words.splice(i,1)
+
+localStorage.setItem("wordSets",JSON.stringify(sets))
+
+loadWords()
+
+}
+
+function toggleFavorite(i){
+
+const sets=JSON.parse(localStorage.getItem("wordSets"))||[]
+
+const word=sets[currentSetIndex].words[i]
+
+word.favorite=!word.favorite
+
+localStorage.setItem("wordSets",JSON.stringify(sets))
+
+loadWords()
+
+}
+
+function speakWord(word){
+
+const msg=new SpeechSynthesisUtterance(word)
+
+speechSynthesis.speak(msg)
+
+}
+
+loadSets()
