0ba89a4690fd936c3c1f3fa34715a9834ea1dff4
emanresuhg <9y4fbsfhhj@privaterelay.appleid.com>
2026-03-14 20:06:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let studyStartTime=null
 function checkPassword(){
 
-const pw = document.getElementById("passwordInput").value;
+const pw = document.getElementById("passwordInput").value
 
 if(pw === "12345"){
 
 sessionStorage.setItem("loggedIn","true")
 
-document.getElementById("lockScreen").style.display="none";
-document.getElementById("app").style.display="block";
+document.getElementById("lockScreen").style.display="none"
+document.getElementById("app").style.display="block"
 
 }else{
 
-alert("Wrong Password");
+alert("Wrong Password")
 
 }
 
