9689d9d945c977af6c048b3cbdaa563acb446868
emanresuhg <9y4fbsfhhj@privaterelay.appleid.com>
2026-03-14 16:57:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,54 @@ alert("Wrong Password");
 }
 
 }
+
+function goHome(){
+
+location.href="../index.html"
+
+}
+
+
+function loadSets(){
+
+const sets = JSON.parse(localStorage.getItem("wordSets")) || []
+
+const list = document.getElementById("setList")
+
+list.innerHTML=""
+
+sets.forEach((set,i)=>{
+
+const div=document.createElement("div")
+
+div.innerHTML=set.name
+
+list.appendChild(div)
+
+})
+
+}
+
+
+function addSet(){
+
+const name = document.getElementById("setName").value
+
+if(!name)return
+
+const sets = JSON.parse(localStorage.getItem("wordSets")) || []
+
+sets.push({name:name,words:[]})
+
+localStorage.setItem("wordSets",JSON.stringify(sets))
+
+loadSets()
+
+}
+
+
+if(document.getElementById("setList")){
+
+loadSets()
+
+}
